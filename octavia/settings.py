f67ceb2971b3a680e07af0e8181cb692cbfce877
Edik1n0 <35901018+Edik1n0@users.noreply.github.com>
2023-09-20 11:24:04 -0500

    final db

--- octavia/settings.py
+++ octavia/settings.py
@@ -135,8 +135,8 @@ DATABASES = {
     'default': {
         #"ENGINE": "django.db.backends.sqlite3",
         #"NAME": BASE_DIR / "db.sqlite3",
-        #'ENGINE': 'django.db.backends.postgresql',
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
+        #'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
